ff1e2db1165c3c966c6f22ddd6be3e29ac5e90df
Aloysius Gilang <gilangpramdya84@gmail.com>
2022-01-23 15:02:03 +0700

    finishing load data

---
+++ loadData/adtData.h
@@ -0,0 +1,8 @@
+#ifndef ADT_DATA_H
+#define ADT_DATA_H
+
+typedef struct {
+   char matrix[]; /* memori tempat penyimpan elemen (container) */
+} ListPos;
+
+#endif
--- loadData/loadData.c
+++ loadData/loadData.c
@@ -2,13 +2,16 @@
 #include "charmachine.h"
 #include "wordmachine.h"
 #include "boolean.h"
+#include "listpos.h"
 
 void load_data (char* filename){
 
     //KAMUS
     int row = 0;
     int col = 0;
-
+    int countChar = 0;
+    int countPattern = 0;
+  
     startWord(filename);
     if (!fileFound) {
         printf("file tidak ditemukan");
@@ -16,17 +19,59 @@ void load_data (char* filename){
     }
 
     while (currentChar != NEWLINE && eot != 1 && currentChar != MARK) {
-        row++;
+        countChar++;
         advWord();
         if (currentChar == NEWLINE) {
             skipNewline();
-            col++;
+            row++;
+        }
+    }
+    
+    col = countChar /row;
+    printf("rows: %d\n",row);
+    printf("cols: %d\n",col);
+
+    char words[row][col];
+
+    startWord(filename);
+    int i = 0; int j = 0;
+    while (currentChar != NEWLINE){
+        words[i][j] = KataToChar(currentWord);
+        advWord();
+        if (currentChar == NEWLINE) {
+            skipNewline();
+            j = 0;
+            i++;
+        } else {
+            j++;
         }
     }
 
-    printf("rows: %d",row);
-    printf("rows: %d",col);
-   
+    //printing words
+    for (i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+        {
+            printf("%c ", words[i][j]);
+        }
+        printf("\n");
+    }
+
+    skipNewline();
+    advWord();
+    ListPos pattern;
+    CreateListPos(&pattern);
+    while (currentChar != MARK){
+        countPattern++;
+        insertLast(&pattern, KataToString(currentWord));
+        if (currentChar == NEWLINE) {
+            skipNewline();
+        }
+        advWord();
+    }
+    printf("patterns:%d\n", countPattern);
+    displayList(pattern);
+
 }
 
 int main (){
